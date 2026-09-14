cfcffa7f60587e37375cb92b4fa1f5c228870a39
Dylan <reborndiscord@gmx.fr>
2026-09-14 18:44:36 +0200

    Add initial implementation of Pascal's triangle and README files

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module 0-pascal_triangle
+"""
+
+
+def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing
+    the Pascal's triangle of n.
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        prev_row = triangle[-1]
+        current_row = [1]
+        for j in range(len(prev_row) - 1):
+            current_row.append(prev_row[j] + prev_row[j + 1])
+
+        current_row.append(1)
+        triangle.append(current_row)
+
+    return triangle
